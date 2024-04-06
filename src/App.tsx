b4443e8ede4238eb6efbb2d7e91ac64e1df27162
Deepen Padwal <63815722+padwaldeepen@users.noreply.github.com>
2024-04-05 22:52:51 -0400

    cleanup files

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 import './App.scss';
 import './styles.scss';
 import { CustomNavbar } from './component/navbar/Navbar';
